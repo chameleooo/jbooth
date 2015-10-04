32639c18497c8954436fbc4ca655d684ca973a3e
Victor LEROY <vicleroy@gmail.com>
2015-10-04 18:52:14 +0200

    It works

--- src/main/java/com/chameleooo/jbooth/service/PrintService.java
+++ src/main/java/com/chameleooo/jbooth/service/PrintService.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.imageio.ImageIO;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.ResolutionSyntax;
+import javax.print.attribute.standard.MediaSizeName;
+import javax.print.attribute.standard.OrientationRequested;
+import javax.print.attribute.standard.PrinterResolution;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.print.Printable;
-import java.awt.print.PrinterException;
-import java.awt.print.PrinterJob;
+import java.awt.print.*;
 import java.io.IOException;
 
 @Service
@@ -19,17 +24,39 @@ public class PrintService {
     public void print(String filename) throws IOException {
         BufferedImage image = ImageIO.read(photoService.getPicture(filename));
         PrinterJob printJob = PrinterJob.getPrinterJob();
-        printJob.setPrintable((graphics, pageFormat, pageIndex) -> {
-            if (pageIndex != 0) {
-                return Printable.NO_SUCH_PAGE;
+
+        int scale = 72;
+        printJob.setPrintable(new Printable() {
+            @Override
+            public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
+                if (pageIndex != 0) {
+                    return Printable.NO_SUCH_PAGE;
+                }
+
+                Graphics2D g2d = (Graphics2D) graphics;
+
+                g2d.scale(scale, scale);
+                g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                        RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+
+                Paper paper = pageFormat.getPaper();
+                paper.setSize(Math.floor(paper.getWidth() + 1), Math.floor(paper.getHeight() + 1));
+                paper.setImageableArea(0, 0, paper.getWidth(), paper.getHeight());
+                pageFormat.setPaper(paper);
+                g2d.drawImage(image, 0, 0, 6, 4, null);
+                return Printable.PAGE_EXISTS;
             }
-            graphics.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
-            return Printable.PAGE_EXISTS;
         });
+
+        PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
+        attributes.add(new PrinterResolution(scale, scale, ResolutionSyntax.DPI));
+        attributes.add(OrientationRequested.LANDSCAPE);
+        attributes.add(MediaSizeName.JAPANESE_POSTCARD);
+
         try {
-            printJob.print();
-        } catch (PrinterException e1) {
-            e1.printStackTrace();
+            printJob.print(attributes);
+        } catch (PrinterException e) {
+            e.printStackTrace();
         }
     }
 }
